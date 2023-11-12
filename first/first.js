991e704933597144f52c47344d5158ea922f3587
Kanishk Munot <munotkanishk@gmail.com>
2023-11-12 12:49:53 +0530

    final commit

--- first/first.js
+++ first/first.js
@@ -1,24 +1,31 @@
-let todoList = []
-displayItems()
+let todoList = [
 
-function addTodo() {
-
+  ];
+  
+  displayItems();
+  
+  function addTodo() {
     let inputElement = document.querySelector('#todo-input');
+    let dateElement = document.querySelector('#todo-date');
     let todoItem = inputElement.value;
-    console.log(todoItem);
-    todoList.push(todoItem);
+    let todoDate = dateElement.value;
+    todoList.push({item: todoItem, dueDate: todoDate});
     inputElement.value = '';
-
+    dateElement.value = '';
     displayItems();
-}
-
-function displayItems() {
-
-    let displayElement = document.querySelector('#todo-items');
+  }
+  
+  function displayItems() {
+    let containerElement = document.querySelector('.todo-container');
+    let newHtml = '';
     for (let i = 0; i < todoList.length; i++) {
-
-        displayElement.innerText = displayElement.innerText + todoList[i]
-
+      let {item, dueDate} = todoList[i];
+      newHtml += `
+        <span>${item}</span>
+        <span>${dueDate}</span>
+        <button class='btn-delete' onclick="todoList.splice(${i}, 1);
+        displayItems();">Delete</button>
+      `;
     }
-    
-}
+    containerElement.innerHTML = newHtml;
+  }
